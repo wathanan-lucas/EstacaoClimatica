6ae8633f0f262720e21641a16788c33a4df93dd0
Rodrigo Bossini <professorbossini@gmail.com>
2023-02-24 22:07:10 -0300

    apostila05(estado): fim da secao 2.7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css'
+// imr
+import React from 'react'
+// imrd
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null,
+      icone: null
+    }
+  }
+  obterEstacao = (data, latitude) => {
+    const anoAtual = data.getFullYear()
+    //23/06
+    const d1 = new Date(anoAtual, 5, 23)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date (anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date (anoAtual, 2, 21)
+    const sul = latitude < 0
+    if (data >= d1 && data < d2)
+      return sul ? 'Inverno' : 'Verão'
+    if (data >= d2 && data < d3)
+      return sul ? 'Primavera' : 'Outono'
+    if (data >= d3 && data < d4)
+      return sul ? 'Verão' : 'Inverno'
+    return sul ? 'Outono' : 'Primavera'
+  }
+  icones = {
+    'Primavera': 'fa-seedling',
+    'Verão': 'fa-umbrella-beach',
+    'Outono': 'fa-tree',
+    'Inverno': 'fa-snowman'
+  }
+  render(){
+    return (
+      <div>
+        Meu app
+      </div>
+    )
+  }
+  
+}
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
